0e9c16de992593e32bf35e731a6991cb3c228d68
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2022-06-23 09:53:49 +0100

    banner change

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '974c815ec3msh3917ee102c792ddp17347djsn54297b053435',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
